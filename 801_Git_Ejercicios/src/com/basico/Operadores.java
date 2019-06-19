dc71ccbef272279283bace2d86ade8abb861e523
Vanadio <vagarme67@gmail.com>
2019-06-19 09:50:38 +0200

    Cambios de numeros

--- 801_Git_Ejercicios/src/com/basico/Operadores.java
+++ 801_Git_Ejercicios/src/com/basico/Operadores.java
@@ -5,8 +5,8 @@ public class Operadores {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int n1=15;
-		int n2=16;
+		int n1=100;
+		int n2=50;
 		
 		System.out.println("La suma es " + (n1+n2));
 		System.out.println("La resta es " + (n1-n2));
